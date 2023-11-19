cf15e2deca853644dfb28eb23d3dacdaa2951ae3
lyamwalburn <lyam567@hotmail.co.uk>
2023-11-19 10:56:34 +0000

    initital setup

---
+++ estate-agent/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Estate Agency</h1>
+      <p>fsdvkndkjn sdvjknsdjkvnsdjk vnwekjnvjkwnefvjknwfv wvnvkwefnvkw</p>
+    </div>
+  );
+}
+
+export default App;
