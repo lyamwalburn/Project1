10d4e3bc073e06be976eec40740383ce45361a37
lyamwalburn <lyam567@hotmail.co.uk>
2023-12-04 17:26:55 +0000

    readded fa icons to property card

--- estate-agent/src/components/PropertyCard.js
+++ estate-agent/src/components/PropertyCard.js
@@ -20,9 +20,9 @@ const PropertyCard = (props) => {
             
             <ul className="list-group list-group-flush">
                 <li className="list-group-item d-flex justify-content-center" key={9345}>
-                    <span className='p-1 mx-2'><FontAwesomeIcon icon={faBath} className='me-2'/>{props.property.bedroom}</span>
-                    {/* <span className='p-1 mx-2'><FontAwesomeIcon icon={faBath} className='me-2'/>{props.property.bathroom}</span>
-                    <span className='p-1 mx-2'><FontAwesomeIcon icon={faTree} className='me-2'/>{props.property.garden}</span> */}
+                    <span className='p-1 mx-2'><FontAwesomeIcon icon={faBed} className='me-2'/>{props.property.bedroom}</span>
+                    <span className='p-1 mx-2'><FontAwesomeIcon icon={faBath} className='me-2'/>{props.property.bathroom}</span>
+                    <span className='p-1 mx-2'><FontAwesomeIcon icon={faTree} className='me-2'/>{props.property.garden}</span>
                 </li>
                 <li className="list-group-item" key={83453}>{props.property.price}</li>
                 <li className="list-group-item" key={72342}>{props.property.status}</li>
