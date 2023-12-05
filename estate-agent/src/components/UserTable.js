3071c58a3d244cfcd5c328b85df83e94df6e8ff7
lyamwalburn <lyam567@hotmail.co.uk>
2023-12-05 13:56:38 +0000

    fixed key error in user table

--- estate-agent/src/components/UserTable.js
+++ estate-agent/src/components/UserTable.js
@@ -1,8 +1,10 @@
-import { useState } from "react";
+import { useId, useState } from "react";
 import { Link } from "react-router-dom";
 const UserTable = (props) => {
 
     const [userToDelete,setuserToDelete] = useState()
+
+    let id = useId()
 
     return ( 
     <div className="container">
@@ -22,38 +24,35 @@ const UserTable = (props) => {
         </thead>
         <tbody>
     {props.users.map(user=>(
-        <>
-        <tr key={user.id} className="text-center">
-            <td>{user.id}</td>
-            <td>{`${user.firstName} ${user.surname}`}</td>
-            <td>{user.address}</td>
-            <td>{user.postcode}</td>
-            <td>{user.phone}</td>
-            <td><Link to={`${props.propertiesLink}${user.id}`}>Properties</Link></td>
-            <td><Link to={`${props.editLink}${user.id}`}>Edit</Link></td>
+        <tr key={id+user.id} className="text-center">
+            <td key={id+1}>{user.id}</td>
+            <td key={id+2}>{`${user.firstName} ${user.surname}`}</td>
+            <td key={id+3}>{user.address}</td>
+            <td key={id+4}>{user.postcode}</td>
+            <td key={id+5}>{user.phone}</td>
+            <td key={id+6}><Link to={`${props.propertiesLink}${user.id}`}>Properties</Link></td>
+            <td key={id+7}><Link to={`${props.editLink}${user.id}`}>Edit</Link></td>
             {/* <td><input type="button" value='Delete' onClick={()=>{props.removeUser(user.id)}} /></td> */}
-            <td><button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal" onClick={()=>setuserToDelete(user.id)}>Delete</button></td>
+            <td key={id+8}><button type="button" className="btn btn-danger" data-bs-toggle="modal" data-bs-target="#deleteModal" onClick={()=>setuserToDelete(user.id)}>Delete</button></td>
         </tr>
- 
-   </>
     ))}
     </tbody>
     </table>
-    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-       <div class="modal-dialog" role="document">
-           <div class="modal-content">
-           <div class="modal-header">
-               <h5 class="modal-title" id="exampleModalLabel">Are you sure?</h5>
-               <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
+    <div className="modal fade" id="deleteModal" tabIndex="-1" role="dialog" aria-labelledby="deleteModalLabel" aria-hidden="true">
+       <div className="modal-dialog" role="document">
+           <div className="modal-content">
+           <div className="modal-header">
+               <h5 className="modal-title" id="deleteModalLabel">Are you sure?</h5>
+               <button type="button" className="close" data-bs-dismiss="modal" aria-label="Close">
                <span aria-hidden="true">&times;</span>
                </button>
            </div>
-           <div class="modal-body">
+           <div className="modal-body">
                This user will be permenantly deleted this cannot be undone.
            </div>
-           <div class="modal-footer">
-               <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
-               <button type="button" class="btn btn-danger" data-bs-dismiss="modal" onClick={()=>{
+           <div className="modal-footer">
+               <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+               <button type="button" className="btn btn-danger" data-bs-dismiss="modal" onClick={()=>{
                 props.removeUser(userToDelete) 
                 }}>Confirm Delete</button>
            </div>
