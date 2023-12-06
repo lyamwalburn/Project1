fd5a37189fbe40ec6779f726ec068f7fb5176311
lyamwalburn <lyam567@hotmail.co.uk>
2023-12-06 12:16:42 +0000

    added keys to boking table

--- estate-agent/src/components/Booking/BookingsTable.js
+++ estate-agent/src/components/Booking/BookingsTable.js
@@ -1,7 +1,9 @@
+import { useId } from "react"
 import { timeSlots } from "../utils"
 
 const BookingsTable = (props) => {
 
+    let id = useId()
     const getBuyerNameString = (booking)=>{
         let buyer = props.buyers.filter(b=>b.id == booking)[0]
         return `${buyer.firstName} ${buyer.surname}`
@@ -23,11 +25,11 @@ const BookingsTable = (props) => {
             </thead>
             <tbody>
                 {props.bookings.filter(b=>b.propertyId == props.property.id).map(booking=>(
-                    <tr>
-                        <td>{booking.date}</td>
-                        <td>{getTimeslotString(booking.timeslot)}</td>
-                        <td>{getBuyerNameString(booking.buyerId)}</td>
-                        <td><button  className='btn btn-danger'onClick={()=>{props.deleteBooking(booking.id)}}>Cancel Booking</button></td>
+                    <tr key={id}>
+                        <td key={id+booking.id}>{booking.date}</td>
+                        <td key={id+3}>{getTimeslotString(booking.timeslot)}</td>
+                        <td key={id+4}>{getBuyerNameString(booking.buyerId)}</td>
+                        <td key={id+5}><button  className='btn btn-danger'onClick={()=>{props.deleteBooking(booking.id)}}>Cancel Booking</button></td>
                     </tr>
                 
                     
