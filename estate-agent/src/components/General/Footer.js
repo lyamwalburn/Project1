f824b408fc6737d64e50efd71d369c3fbd93fc8d
lyamwalburn <lyam567@hotmail.co.uk>
2023-12-06 20:10:31 +0000

    updated footer text

--- estate-agent/src/components/General/Footer.js
+++ estate-agent/src/components/General/Footer.js
@@ -11,36 +11,16 @@ const Footer = () => {
               <div className="container text-center text-md-start mt-5">
                 <div className="row mt-3">
                   <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-                    <h6 className="text-uppercase fw-bold">Company name</h6>
+                    <h6 className="text-uppercase fw-bold">B&G Estates</h6>
                     <hr
                         className="mb-4 mt-0 d-inline-block mx-auto"
                         
                         />
                     <p>
-                      Here you can use rows and columns to organize your footer
-                      content. Lorem ipsum dolor sit amet, consectetur adipisicing
-                      elit.
+                      Supplying first class sales services for the real estate value since 2023.
                     </p>
                   </div>
-                  <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-                    <h6 className="text-uppercase fw-bold">Products</h6>
-                    <hr
-                        className="mb-4 mt-0 d-inline-block mx-auto"
-                        
-                        />
-                    <p>
-                      <a href="#!" className="text-white">MDBootstrap</a>
-                    </p>
-                    <p>
-                      <a href="#!" className="text-white">MDWordPress</a>
-                    </p>
-                    <p>
-                      <a href="#!" className="text-white">BrandFlow</a>
-                    </p>
-                    <p>
-                      <a href="#!" className="text-white">Bootstrap Angular</a>
-                    </p>
-                  </div>
+                  
                   <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
                     <h6 className="text-uppercase fw-bold">Useful links</h6>
                     <hr
@@ -48,16 +28,13 @@ const Footer = () => {
                        
                         />
                     <p>
-                      <a href="#!" className="text-white">Your Account</a>
+                      <a href="#" className="text-white">Properties</a>
                     </p>
                     <p>
-                      <a href="#!" className="text-white">Become an Affiliate</a>
+                      <a href="#" className="text-white">Our Sellers</a>
                     </p>
                     <p>
-                      <a href="#!" className="text-white">Shipping Rates</a>
-                    </p>
-                    <p>
-                      <a href="#!" className="text-white">Help</a>
+                      <a href="#" className="text-white">Help</a>
                     </p>
                   </div>
                   <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
@@ -66,10 +43,10 @@ const Footer = () => {
                         className="mb-4 mt-0 d-inline-block mx-auto"
                         
                         />
-                    <p><i className="fas fa-home mr-3"></i> New York, NY 10012, US</p>
-                    <p><i className="fas fa-envelope mr-3"></i> info@example.com</p>
-                    <p><i className="fas fa-phone mr-3"></i> + 01 234 567 88</p>
-                    <p><i className="fas fa-print mr-3"></i> + 01 234 567 89</p>
+                    <p><i className="mr-3"></i> B&G Estates, Perth Road Dundee, DD1 6PJ</p>
+                    <p><i className="mr-3"></i> info@bandg.com</p>
+                    <p><i className="mr-3"></i> 01382 123456</p>
+                    <p><i className="mr-3"></i> 01382 987654</p>
                   </div>
                 </div>
               </div>
@@ -78,9 +55,9 @@ const Footer = () => {
                  className="text-center p-3"
                 
                  >
-              © 2020 Copyright:
+              2023 B&G Estates &nbsp;
               <a className="text-white" href="https://mdbootstrap.com/"
-                 >MDBootstrap.com</a
+                 >BGEstates.co.uk</a
                 >
             </div>
           </footer>
